b8ca1322032fbb8d7b49e514c16843372af311ad
Julien Pivotto <roidelapluie@inuits.eu>
2015-08-06 18:14:39 +0200

    Tryout some spec test

---
+++ spec/defines/fooacl_conf_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'fooacl::conf', :type => :define do
+  let :title do
+    '/tmp/test.acl'
+  end
+
+  context 'with an array as permissions' do
+    let :params do {
+      permissions => ['user:myuser:rwx', 'group:mygroup:rwx'],
+    } end
+    it should contain_concat__fragment('/tmp/test.acl')
+  end
+end
+
